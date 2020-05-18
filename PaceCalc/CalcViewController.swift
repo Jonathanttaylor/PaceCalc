68bb274be034c3129645109dd7128c37ead4623a
Jonathan Taylor <jttaylor@mun.ca>
2020-05-18 21:02:57 -0230

    Create tab view

--- PaceCalc/CalcViewController.swift
+++ PaceCalc/CalcViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CalcViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+        
     }
 
 
---
+++ PaceCalc/ConvertViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ConvertViewController.swift
+//  PaceCalc
+//
+//  Created by Jonathan Taylor on 2020-05-18.
+//  Copyright © 2020 Jonathan Taylor. All rights reserved.
+//
+
+import UIKit
+
+class ConvertViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+}
+
