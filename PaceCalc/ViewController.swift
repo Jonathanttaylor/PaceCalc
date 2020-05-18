65e435fd2033312ece02e0e4573168bb761c5f6f
Jonathan Taylor <jttaylor@mun.ca>
2020-05-18 20:43:09 -0230

    Initial Commit

---
+++ PaceCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PaceCalc
+//
+//  Created by Jonathan Taylor on 2020-05-18.
+//  Copyright © 2020 Jonathan Taylor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
