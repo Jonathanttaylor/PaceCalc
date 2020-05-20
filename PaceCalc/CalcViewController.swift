1c29ee1642a007a62d34f913581030f02adca0ae
Jonathan Taylor <jttaylor@mun.ca>
2020-05-20 17:33:08 -0230

    Add ability to close textfield keyboards

--- PaceCalc/CalcViewController.swift
+++ PaceCalc/CalcViewController.swift
@@ -8,13 +8,67 @@
 
 import UIKit
 
-class CalcViewController: UIViewController {
+class CalcViewController: UIViewController, UITextFieldDelegate {
 
+    // Initalizing Text Fields
+    @IBOutlet weak var timeHr: UITextField!
+    @IBOutlet weak var timeMin: UITextField!
+    @IBOutlet weak var timeSec: UITextField!
+    @IBOutlet weak var dist: UITextField!
+    @IBOutlet weak var paceMin: UITextField!
+    @IBOutlet weak var paceSec: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configTapGesture()
+        configTextFields()
+    }
+    
+    // Removing keyboard when outside test field is tapped
+    private func configTapGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(CalcViewController.handleTap))
+        view.addGestureRecognizer(tapGesture)
+    }
+    @objc func handleTap() {
+        view.endEditing(true)
+    }
+    
+    private func configTextFields() {
+        timeHr.delegate = self
+        timeMin.delegate = self
+        timeSec.delegate = self
+        paceMin.delegate = self
+        paceSec.delegate = self
         
     }
+    
+    @IBAction func distPressed(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func pacePressed(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calcPressed(_ sender: Any) {
+        view.endEditing(true)
+    }
 
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // Setting test field character limit to two
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
 
+        let currentCharacterCount = textField.text?.count ?? 0
+        if range.length + range.location > currentCharacterCount {
+            return false
+        }
+        let newLength = currentCharacterCount + string.count - range.length
+        return newLength <= 2
+    }
+    
 }
 
--- PaceCalc/ConvertViewController.swift
+++ PaceCalc/ConvertViewController.swift
@@ -8,11 +8,62 @@
 
 import UIKit
 
-class ConvertViewController: UIViewController {
+class ConvertViewController: UIViewController, UITextFieldDelegate {
+    
+    // Initalizing Text Fields
+    @IBOutlet weak var paceMin_1: UITextField!
+    @IBOutlet weak var paceSec_1: UITextField!
+    @IBOutlet weak var paceMin_2: UITextField!
+    @IBOutlet weak var paceSec_2: UITextField!
+    @IBOutlet weak var dist_1: UITextField!
+    @IBOutlet weak var dist2: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configTapGesture()
+        configTextFields()
     }
+    
+    // Removing keyboard when outside test field is tapped
+    private func configTapGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(CalcViewController.handleTap))
+        view.addGestureRecognizer(tapGesture)
+    }
+    @objc func handleTap() {
+        view.endEditing(true)
+    }
+    
+    private func configTextFields() {
+        paceMin_1.delegate = self
+        paceSec_1.delegate = self
+        paceMin_2.delegate = self
+        paceSec_2.delegate = self
+    }
+    
+    @IBAction func ConvertOnePressed(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func ConvertTwoPressed(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func CalcPressed(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    // Setting test field character limit to two
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+
+        let currentCharacterCount = textField.text?.count ?? 0
+        if range.length + range.location > currentCharacterCount {
+            return false
+        }
+        let newLength = currentCharacterCount + string.count - range.length
+        return newLength <= 2
+    }
+    
 
 }
 
